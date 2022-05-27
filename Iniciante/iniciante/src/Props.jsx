9c008cb69be3f773d662cf3df82cde05fce2cb07
Daniel Custodio <play.daniel@hotmail.com>
2022-05-27 19:55:13 -0300

    Props

---
+++ Iniciante/iniciante/src/Props.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Input from './Input'
+
+const Props = () => {
+  return (
+    <>
+      <Input id="email" type="text" label="Email" required />
+      <Input id="password" type="password" label="Passowrd" />
+    </>
+
+  )
+}
+
+export default Props
