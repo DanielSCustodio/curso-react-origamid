13b45d035558ae6d56dcb5a8d0a78b959307e849
Daniel Custodio <play.daniel@hotmail.com>
2022-07-18 17:50:59 -0300

    [22] - Aplicação de useContext - completo

--- hooks/use-context/src/Context/UserProvider.jsx
+++ hooks/use-context/src/Context/UserProvider.jsx
@@ -3,13 +3,23 @@ import UserContext from './UserContext';
 
 const UserProvider = ({children}) => {
   const [count, setCount] = React.useState(0);
+  const [name, setName] = React.useState('');
 
-  const { name } = JSON.parse(localStorage.getItem('user'));
+  React.useEffect(()=>{
+    localStorage.setItem('name', 'Daniel');
+    const nameLocal = localStorage.getItem('name')
+    setName(nameLocal)
+  }, [])
+
+  const subtraction = () => {
+    setCount(count => count - 1)
+  }
 
   let values = {
     count,
     setCount,
     name,
+    subtraction
   }
   return (
     <UserContext.Provider value={values}>
